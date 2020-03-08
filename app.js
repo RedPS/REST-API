30a6da44568c1d44e8c8f993abe1c0d5b0e43d36
redps <psafaei@nevada.unr.edu>
2020-03-07 22:31:46 -0800

    updated app to be more organized

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv/config");
+
+const newpageroute = require("./routes/newpage");
+
+app.use("/newpage", newpageroute);
 
 //set the middlewares
 app.use("/newpage", () => {
@@ -11,9 +18,12 @@ app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
-app.get("/newpage", (req, res) => {
-  res.send("New Page");
-});
+//connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB")
+);
 
 //listen to server port 3000
 app.listen(3000);

38aa4dd328c7e5634e69402403126a44f157fd1a
redps <psafaei@nevada.unr.edu>
2020-03-20 20:34:43 -0700

    formatting issue?!

--- routes/newpage.js
+++ routes/newpage.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
+const Page = require("../models/Newpage");
 
 router.get("/", (req, res) => {
   res.send("New Page");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;

5e2e2d903b18891e25f69d45fc1b0629f8f3791d
redps <psafaei@nevada.unr.edu>
2020-04-02 21:55:26 -0700

    routes updated

--- routes/newpage.js
+++ routes/newpage.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const Page = require("../models/Newpage");
+const Page = require("../models/_Newpage");
 
 router.get("/", (req, res) => {
   res.send("New Page");
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  const page = new Page({
+    title: req.body.title,
+    description: req.body.description
+  });
+  page
+    .save()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ message: err });
+    });
 });
 
 module.exports = router;
